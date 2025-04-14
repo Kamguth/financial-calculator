2c4225da696863d4375088cb27ff4ce0ffde2adf
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-04-14 17:53:24 -0400

    Finished financial calculators

--- Financial-Calculators/src/main/java/com/pluralsight/AnnuityValueCalculator.java
+++ Financial-Calculators/src/main/java/com/pluralsight/AnnuityValueCalculator.java
@@ -7,7 +7,22 @@ public class AnnuityValueCalculator {
         Scanner input = new Scanner(System.in);
 
 
+        System.out.println("Hello, please enter all info when prompted");
 
+        System.out.print("Enter expected payout: ");
+        double payout = input.nextDouble();
+        System.out.print("Enter expected interest rate: ");
+        double interestRate = input.nextDouble();
+        System.out.print("Enter expected years until payout:" );
+        int yearsUntilPayout = input.nextInt();
+
+        double monthlyRate = interestRate / 100 / 12;
+        int totalPayments = yearsUntilPayout * 12;
+
+        double currentValue = payout * ((1 - Math.pow(1 + monthlyRate, -totalPayments)) / monthlyRate);
+
+
+        System.out.printf("Current value of annuity: $%.2f", currentValue);
 
     }
 }
